25b3aa0ef02ea002931e13c9ac237a9c2386c53d
LJLJLJ <47559665+GogoZin@users.noreply.github.com>
2025-05-28 08:40:31 +0800

    Update dpf.py

--- dpf.py
+++ dpf.py
@@ -134,7 +134,7 @@ def check_proxy(proxy): # 檢測proxy的TCP connection跟HTTP REQUESTS
 
     s = socks.socksocket()
     s.set_proxy(socks.SOCKS5, proxy_ip, proxy_port)
-    s.settimeout(5)
+    s.settimeout(2)
 
     try:
         s.connect((host, port))
