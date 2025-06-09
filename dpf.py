48860a87bf6a91dc956b11c815673ce228332f12
LJLJLJ <47559665+GogoZin@users.noreply.github.com>
2025-06-09 14:40:44 +0800

    新增兩個

--- dpf.py
+++ dpf.py
@@ -230,6 +230,7 @@ def headerHandle(): #封包標頭處理
         origin += f"https://{host}\r\n"
     else:
         origin += f"http://{host}\r\n"
+    uir = f"Upgrade-Insecure-Requests: 1\r\n"
 
     # Http 安全性標頭, 大部分主流的瀏覽器都支援了, 是判斷為正常流量 或是機器人的標準之一
     # 新的站點大部分都是預設啟用sec的, 也就是沒有sec的都會被識別為惡意流量
@@ -246,10 +247,11 @@ def headerHandle(): #封包標頭處理
     sec += f"Sec-Fetch-Mode: cors\r\n"
     sec += f"Sec-Fetch-Site: same-origin\r\n"
     sec += f"Sec-Gpc: 1\r\n"
+    sec += f"Sec-Fetch-User: ?1\r\n"
 
-    header = conn + accept + referer + useragent + x_for + cache + pri + origin
+    header = conn + accept + referer + useragent + x_for + cache + pri + origin + uir
     if brute: #如果啟用brute 就最大程度減少標頭 只留關鍵標頭
-        header = conn + cache + useragent
+        header = conn + cache + referer + useragent + uir
     if cdn: #如果是bypass模式 那就必須加入sec
         header +=sec
     return header #回傳處理好的標頭
